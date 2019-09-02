9f17146fe968f4da70908432624c230c7d0d3bf9
Trace <lukaszdabrowski881@gmail.com>
2019-09-03 00:20:34 +0100

    added build and run scripts

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+cd ModdedMinecraftClub.MemberBot
+dotnet publish -c Release
+cd ..
+
+if [ -d Bot ]
+then 
+    cd Bot
+    rm -r *
+    cd ..
+else
+    mkdir Bot
+fi
+
+cp -r ModdedMinecraftClub.MemberBot/ModdedMinecraftClub.MemberBot.Bot/bin/Release/netcoreapp2.2/publish/* Bot/
+
+printf "\n\nDone.\n"
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+cd Bot
+
+dotnet ModdedMinecraftClub.MemberBot.Bot.dll
