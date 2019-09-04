c3fc394972fa7b1d4793649b79b494ed03a1b3a8
Trace <lukaszdabrowski881@gmail.com>
2019-09-04 04:52:04 +0100

    Added the sql data access

--- ModdedMinecraftClub.MemberBot/ModdedMinecraftClub.MemberBot.Bot/DatabaseConnection.cs
+++ ModdedMinecraftClub.MemberBot/ModdedMinecraftClub.MemberBot.Bot/DatabaseConnection.cs
@@ -12,6 +12,13 @@ namespace ModdedMinecraftClub.MemberBot.Bot
         void ExecuteRawNonQuery<T>(string sql, T objectToInsert);
         List<T> ExecuteRawQuery<T>(string sql);
         bool DoesTableExist();
+        void CreateTable();
+        List<Application> GetAllPending();
+        Application GetById(int applicationId);
+        List<Application> GetLast20Approved();
+        List<Application> GetLast20Rejected();
+        void MarkAsApproved(int applicationId);
+        void MarkAsRejected(int applicationId);
     }
     
     public class DatabaseConnection : IDisposable, IDatabaseConnection
@@ -58,7 +65,68 @@ namespace ModdedMinecraftClub.MemberBot.Bot
 
         public void CreateTable()
         {
+            const string sql =
+                "create table applications\n(\n\tAppId int not null auto_increment,\n\tAppStatus int not null,\n\tAppTime varchar(50) null,\n\tAuthorName varchar(50) not null,\n\tAuthorDiscordId long null,\n\tMessageContent varchar(800) null,\n    MessageUrl varchar(250) not null,\n\tImageUrl varchar(250) null,\n\tprimary key (AppId)\n);";
+
+            _connection.Execute(sql);
+        }
+
+        public void InsertNewApplication(Application application)
+        {
+            const string sql =
+                "insert into applications (AppStatus, AppTime, AuthorName, AuthorDiscordId, MessageContent, MessageUrl, ImageUrl) values (@AppStatus, @AppTime, @AuthorName, @AuthorDiscordId, @MessageContent, @MessageUrl, @ImageUrl)";
+
+            _connection.Execute(sql, application);
+        }
+
+        public List<Application> GetAllPending()
+        {
+            const string sql =
+                "select * from applications where AppStatus = 0 order by AppId";
+
+            return _connection.Query<Application>(sql).ToList();
+        }
+        
+        public Application GetById(int applicationId)
+        {
+            const string sql =
+                "select * from applications where AppId = @AppId";
             
+            var l = _connection.Query<Application>(sql, new { AppId = applicationId }).ToList();
+
+            return !l.Any() ? null : l[0];
+        }
+        
+        public List<Application> GetLast20Approved()
+        {
+            const string sql =
+                "select * from applications where AppStatus = 1 order by AppId limit 20";
+            
+            return _connection.Query<Application>(sql).ToList();
+        }
+        
+        public List<Application> GetLast20Rejected()
+        {
+            const string sql =
+                "select * from applications where AppStatus = 2 order by AppId limit 20";
+            
+            return _connection.Query<Application>(sql).ToList();
+        }
+
+        public void MarkAsApproved(int applicationId)
+        {
+            const string sql =
+                "update applications\nset AppStatus = 1\nwhere AppId = @AppId";
+
+            _connection.Execute(sql, new { AppId = applicationId });
+        }
+
+        public void MarkAsRejected(int applicationId)
+        {
+            const string sql =
+                "update applications set AppStatus = 2 where AppId = @AppId";
+
+            _connection.Execute(sql, new { AppId = applicationId });
         }
 
         public void Dispose()
