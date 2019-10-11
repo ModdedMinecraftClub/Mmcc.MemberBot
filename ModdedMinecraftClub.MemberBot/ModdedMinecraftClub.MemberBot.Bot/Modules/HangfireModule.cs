e461d9d3daa28e35f8380af8d1cd47c1fa777c76
Trace <lukaszdabrowski881@gmail.com>
2019-10-11 06:33:02 +0100

    group commands into better modules

---
+++ ModdedMinecraftClub.MemberBot/ModdedMinecraftClub.MemberBot.Bot/Modules/HangfireModule.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Threading.Tasks;
+using ConsoleTables;
+using Discord;
+using Discord.Commands;
+using Hangfire;
+using ModdedMinecraftClub.MemberBot.Bot.Jobs.Models;
+
+namespace ModdedMinecraftClub.MemberBot.Bot.Modules
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    public class HangfireModule : ModuleBase<SocketCommandContext>
+    {
+        private static CultureInfo EnUs => new CultureInfo("en-US");
+        private static TimeZoneInfo Est => TimeZoneInfo.FindSystemTimeZoneById(Program.Config.EstTimeZoneString);
+
+        #region Staff Only
+
+        [Command("jobs", RunMode = RunMode.Async)]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        public async Task Jobs(string arg)
+        {
+            arg = arg.ToLower();
+            
+            if (arg.Equals(JobStatus.Scheduled.ToString().ToLower()))
+            {
+                using (var c = new DatabaseConnection())
+                {
+                    var table = new ConsoleTable("Id", "Method", "Created At", "Scheduled For");
+                    var jobDtos = c.GetJobs(JobStatus.Scheduled);
+                    var jobs = new List<ScheduledJob>();
+
+                    foreach (var jobDto in jobDtos)
+                    {
+                        var job = new ScheduledJob(jobDto);
+                        job.Parse();
+                        jobs.Add(job);
+                    }
+
+                    foreach (var job in jobs)
+                    {
+                        table.AddRow(job.Id, job.Method,
+                            $"{TimeZoneInfo.ConvertTimeFromUtc(job.CreatedAt, Est).ToString(EnUs)} EST",
+                            $"{TimeZoneInfo.ConvertTimeFromUtc(job.EnqueueAt, Est).ToString(EnUs)} EST");
+                    }
+
+                    await Context.Channel.SendMessageAsync($"```json\n{table.ToMinimalString()}\n```");
+                }
+            }
+            else if (arg.Equals(JobStatus.Succeeded.ToString().ToLower()) || arg.Equals("completed"))
+            {
+                using (var c = new DatabaseConnection())
+                {
+                    var table = new ConsoleTable("Id", "Method", "Created At", "Completed At");
+                    var jobDtos = c.GetJobs(JobStatus.Succeeded);
+                    var jobs = new List<SucceededJob>();
+
+                    foreach (var jobDto in jobDtos)
+                    {
+                        var job = new SucceededJob(jobDto);
+                        job.Parse();
+                        jobs.Add(job);
+                    }
+
+                    foreach (var job in jobs)
+                    {
+                        table.AddRow(job.Id, job.Method,
+                            $"{TimeZoneInfo.ConvertTimeFromUtc(job.CreatedAt, Est).ToString(EnUs)} EST",
+                            $"{TimeZoneInfo.ConvertTimeFromUtc(job.SucceededAt, Est).ToString(EnUs)} EST");
+                    }
+
+                    await Context.Channel.SendMessageAsync($"```json\n{table.ToMinimalString()}\n```");
+                }
+            }
+            else if (arg.Equals(JobStatus.Failed.ToString().ToLower()))
+            {
+                using (var c = new DatabaseConnection())
+                {
+                    var table = new ConsoleTable("Id", "Method", "Created At");
+                    var jobDtos = c.GetJobs(JobStatus.Succeeded);
+                    var jobs = new List<FailedJob>();
+
+                    foreach (var jobDto in jobDtos)
+                    {
+                        var job = new FailedJob(jobDto);
+                        job.Parse();
+                        jobs.Add(job);
+                    }
+
+                    foreach (var job in jobs)
+                    {
+                        table.AddRow(job.Id, job.Method,
+                            $"{TimeZoneInfo.ConvertTimeFromUtc(job.CreatedAt, Est).ToString(EnUs)} EST");
+                    }
+
+                    await Context.Channel.SendMessageAsync($"```json\n{table.ToMinimalString()}\n```");
+                }
+            }
+        }
+
+        [Command("deletejob", RunMode = RunMode.Async)]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        public async Task DeleteJob(string jobId)
+        {
+            BackgroundJob.Delete(jobId);
+
+            await Context.Channel.SendMessageAsync($":white_check_mark: Successfully deleted the job with id `{jobId}`");
+        }
+
+        #endregion
+    }
+}
---
+++ ModdedMinecraftClub.MemberBot/ModdedMinecraftClub.MemberBot.Bot/Modules/VipModule.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Hangfire;
+using ModdedMinecraftClub.MemberBot.Bot.Jobs;
+
+namespace ModdedMinecraftClub.MemberBot.Bot.Modules
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    public class VipModule : ModuleBase<SocketCommandContext>
+    {
+        #region Staff Only
+
+        [Command("vip", RunMode = RunMode.Async)]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        public async Task Vip(string ign, SocketGuildUser guildUser)
+        {
+            var channels = Context.Guild.TextChannels;
+            var polychatChannel = channels.First(channel => channel.Name.Equals(Program.Config.Discord.ChannelNames.Polychat));
+            
+            // Give VIP on all servers
+            await polychatChannel.SendMessageAsync($"!exec <all> ranks set {ign} vip");
+            
+            // Give VIP role on Discord
+            var roles = Context.Guild.Roles;
+            var vipRole = roles.First(r => r.Name.Equals("Vip"));
+            await guildUser.AddRoleAsync(vipRole);
+
+            var dateNow = DateTime.UtcNow;
+            var vipExpiry = dateNow.AddMonths(1);
+            
+            // Schedule a job that will take VIP away
+            BackgroundJob.Schedule(() => TakeVipAwayJob.Execute(guildUser.Id, Context.Guild.Id, ign), vipExpiry);
+            
+            // Info message
+            await Context.Channel.SendMessageAsync($":white_check_mark: Gave VIP to {ign} and scheduled a VIP job for next month.");
+        }
+
+        #endregion
+    }
+}
