1e6d23d5425251adab9754f818a67d759e2ca564
Trace <lukaszdabrowski881@gmail.com>
2020-06-09 02:13:15 +0100

    delete old code and add dev script

--- ModdedMinecraftClub.MemberBot/ModdedMinecraftClub.MemberBot.Bot/Models/ConfigRoot.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ModdedMinecraftClub.MemberBot.Bot.Models
-{
-    public class ConfigRoot
-    {
-        public Mysql Mysql { get; set; }
-        public Discord Discord { get; set; }
-    }
-}
---
+++ dev.ps1
@@ -0,0 +1,5 @@
+$ErrorActionPreference = "Stop"
+
+.\build.ps1
+
+dotnet .\Bot\ModdedMinecraftClub.MemberBot.Bot.dll
