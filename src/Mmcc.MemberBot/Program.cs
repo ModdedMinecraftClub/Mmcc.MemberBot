1fafd2833243370e430f5c5ca921223e6ebd8124
Trace <lukaszdabrowski881@gmail.com>
2020-08-18 08:05:13 +0100

    add hosting

--- src/Mmcc.MemberBot/Program.cs
+++ src/Mmcc.MemberBot/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace Mmcc.MemberBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(builder =>
+                {
+                    builder.AddCommandLine(args);
+                })
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddJsonFile("appsettings.json", optional: false);
+                })
+                .ConfigureLogging((context, builder) =>
+                {
+                    builder.ClearProviders();
+
+                    if (context.HostingEnvironment.IsDevelopment())
+                    {
+                        builder.AddDebug();
+                    }
+
+                    builder.AddConsole(o =>
+                    {
+                        if (context.HostingEnvironment.IsProduction())
+                        {
+                            o.DisableColors = true;
+                        }
+
+                        o.Format = ConsoleLoggerFormat.Systemd;
+                        o.TimestampFormat = "[dd/MM/yyyy HH:mm:ss] ";
+                    });
+                })
+                .ConfigureServices((context, services) =>
+                {
+
+                });
+
+            using var builtHost = host.Build();
+            await builtHost.StartAsync();
+            await builtHost.WaitForShutdownAsync();
         }
     }
 }
