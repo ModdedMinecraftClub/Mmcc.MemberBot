a72a4c5f6d959b19b27ba06b02d48d64d37f7451
Trace <lukaszdabrowski881@gmail.com>
2020-08-25 20:01:52 +0100

    make link nullable

--- src/Mmcc.MemberBot.Core/Models/SimpleCommand.cs
+++ src/Mmcc.MemberBot.Core/Models/SimpleCommand.cs
@@ -1,11 +1,12 @@
-﻿namespace Mmcc.MemberBot.Core.Models
+﻿#nullable enable
+namespace Mmcc.MemberBot.Core.Models
 {
     public class SimpleCommand
     {
-        public string Name { get; set; }
-        public string Summary { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = null!;
+        public string Summary { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
         public string? Link { get; set; }
     }
 }
